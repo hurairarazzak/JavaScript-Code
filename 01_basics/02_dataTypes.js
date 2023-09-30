58980f01bb0a292a852cf66e0b771982c790b3ea
Huraira Razzak <142255443+hurairarazzak@users.noreply.github.com>
2023-09-30 11:31:58 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3) // code redability should be high
+
+console.log("Huraira")
+
+let name = "Huraira"
+let age = 14
+let loggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undifined
+console.log(typeof null); // object
